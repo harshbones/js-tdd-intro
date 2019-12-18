3de9c35d1e211ad247a773fa16ce4134d1b970fa
harshbones <davidrangeard@gmail.com>
2019-12-18 16:44:24 +0100

    added function

--- capitalizeFirstLetters.js
+++ capitalizeFirstLetters.js
@@ -2,12 +2,19 @@ const assert = require('assert');
 
 // CODE A TESTER ENTRE CES LIGNES
 
+function capitalizeFirstLetters(input) {
+    if (input.length != 0) {
+        return input.split(' ').map(x => x[0].toUpperCase()+x.slice(1)).join(' ')
+    } else {
+    return input;
+    }
+}
 
 // CODE A TESTER ENTRE CES LIGNES
 
 assert.strictEqual(typeof capitalizeFirstLetters, 'function');
-assert.strictEqual(capitalizeFirst.length, 1);
-assert.strictEqual(capitalizeFirst('javaScript'), 'JavaScript');
-assert.strictEqual(capitalizeFirst('a'), 'A');
-assert.strictEqual(capitalizeFirst('i am learning TDD'), 'I Am Learning TDD');
-assert.strictEqual(capitalizeFirst(''), '');
+assert.strictEqual(capitalizeFirstLetters.length, 1);
+assert.strictEqual(capitalizeFirstLetters('javaScript'), 'JavaScript');
+assert.strictEqual(capitalizeFirstLetters('a'), 'A');
+assert.strictEqual(capitalizeFirstLetters('i am learning TDD'), 'I Am Learning TDD');
+assert.strictEqual(capitalizeFirstLetters(''), '');
